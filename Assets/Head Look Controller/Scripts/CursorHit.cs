36824039d130a487f129ef585ba5cfae3c4a73d4
Zhaorui Chen <zhaorui@ualberta.ca>
2017-04-07 01:27:08 -0600

    Add gesture control

--- Assets/Head Look Controller/Scripts/CursorHit.cs
+++ Assets/Head Look Controller/Scripts/CursorHit.cs
@@ -1,3 +1,7 @@
+/* 
+ * Originally from the asset Head Look Controller
+ * Modified by Zhaorui in 2017
+ * */
 using UnityEngine;
 using System.Collections;
 
@@ -8,18 +12,6 @@ public class CursorHit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        /*
-		if (Input.GetKey(KeyCode.UpArrow))
-			offset += Time.deltaTime;
-		if (Input.GetKey(KeyCode.DownArrow))
-			offset -= Time.deltaTime;
-		
-		Ray cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if (Physics.Raycast(cursorRay, out hit)) {
-			transform.position = hit.point + offset * Vector3.up;
-		}
-		*/
 		headLook.target = transform.position;
 	}
 }
